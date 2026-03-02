614598171b2eff8c0ac084105f3aa152bddd8df4
ezek9292 <ezekdonkor@gmail.com>
2026-03-02 15:31:01 +0000

    cors fixed

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,3 +1,3 @@
-const FALLBACK_API_BASE_URL = 'http://localhost:5000';
+const FALLBACK_API_BASE_URL = 'https://shoppingapp-froy.onrender.com';
 
 export const API_BASE_URL = (process.env.REACT_APP_API_BASE || FALLBACK_API_BASE_URL).replace(/\/+$/, '');
