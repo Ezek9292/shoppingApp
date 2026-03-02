44b3330a9cb4c70f0018491674a093db8ef0a7c7
ezek9292 <ezekdonkor@gmail.com>
2026-03-02 16:18:43 +0000

    still fixing cors

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -23,27 +23,27 @@ const Cart = ({ cartItems, removeFromCart, updateQuantity }) => {
       <h2>Shopping Cart</h2>
       <div className="cart-items">
         {cartItems.map(item => (
-          <div key={item.id} className="cart-item">
+          <div key={item._id || item.id} className="cart-item">
             <div className="item-details">
               <h4>{item.name}</h4>
               <p>GHS {item.price.toFixed(2)}</p>
             </div>
             <div className="item-quantity">
-              <button onClick={() => updateQuantity(item.id, item.quantity - 1)} type="button">-</button>
+              <button onClick={() => updateQuantity(item._id || item.id, item.quantity - 1)} type="button">-</button>
               <input
                 type="number"
                 value={item.quantity}
-                onChange={(e) => updateQuantity(item.id, parseInt(e.target.value, 10) || 0)}
+                onChange={(e) => updateQuantity(item._id || item.id, parseInt(e.target.value, 10) || 0)}
                 min="1"
               />
-              <button onClick={() => updateQuantity(item.id, item.quantity + 1)} type="button">+</button>
+              <button onClick={() => updateQuantity(item._id || item.id, item.quantity + 1)} type="button">+</button>
             </div>
             <div className="item-total">
               GHS {(item.price * item.quantity).toFixed(2)}
             </div>
             <button
               className="remove-btn"
-              onClick={() => removeFromCart(item.id)}
+              onClick={() => removeFromCart(item._id || item.id)}
               type="button"
             >
               Remove
